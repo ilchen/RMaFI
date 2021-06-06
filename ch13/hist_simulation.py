24e7f408a6d8e6d1ee54042c80b9254dcc41eb71
Andrei ILchenko <andrei.ilchenko@gmail.com>
2021-06-06 18:47:43 +0200

    Implemented 13.7 using volatility scaling for the portfolio

--- ch13/hist_simulation.py
+++ ch13/hist_simulation.py
@@ -5,7 +5,7 @@ import pandas as pd
 import locale
 import datetime
 from math import sqrt
-
+import matplotlib.pyplot as plt
 
 class HistSimulation:
     """
@@ -77,7 +77,7 @@ class HistSimulation:
         print("Exercise 13.6. One day ES with a confidence level of %2.1f%% and \u03BB=%1.2f: %s"
               % (VaR_conf * 100, lbd, locale.currency(es * self.scale_factor, grouping=True)))
 
-    def exercise_13_7(self, lbd):
+    def exercise_13_7_old(self, lbd):
         djia_var = self.data_pct.DJIA.var()
         ftse_var = self.data_pct.FTSE.var()
         cac_var  = self.data_pct.CAC.var()
@@ -89,6 +89,8 @@ class HistSimulation:
         _data_pct['CAC_Var']  = cac_var
         _data_pct['Nikkei_Var'] = nikkei_var
         _data_pct = _data_pct[['DJIA', 'DJIA_Var', 'FTSE', 'FTSE_Var', 'CAC', 'CAC_Var', 'Nikkei', 'Nikkei_Var']]
+
+        # Adding an extra row to calculate volatilities on the day after the last one in the sample -- 26th Sept 2008
         extra_row = DataFrame(np.zeros((1,8)), index=[_data_pct.index[len(_data_pct) - 1]+datetime.timedelta(days=1)],
                               columns=_data_pct.columns)
         _data_pct  = pd.concat([_data_pct, extra_row], axis=0)
@@ -104,7 +106,11 @@ class HistSimulation:
         nikkei_var = _data_pct.Nikkei_Var[len(self.data) - 1]
 
         _data_pct2 = pd.DataFrame(index=_data_pct.index, columns=self.data.columns)
-        _data_pct2.drop(_data_pct2.index[-1])
+
+        # The last row for 26th September is not needed anymore
+        _data_pct2.drop(_data_pct2.index[-1], inplace=True)
+
+        # Applying formula 13.2
         for i in range(1, len(self.data)):
             _data_pct2.DJIA[i - 1] = (self.data.DJIA[i - 1] + (self.data.DJIA[i] - self.data.DJIA[i - 1]) * sqrt(djia_var) / sqrt(
                 _data_pct.DJIA_Var[i - 1])) / self.data.DJIA[i - 1]
@@ -114,18 +120,73 @@ class HistSimulation:
                 _data_pct.CAC_Var[i - 1])) / self.data.CAC[i - 1]
             _data_pct2.Nikkei[i - 1] = (self.data.Nikkei[i - 1] + (self.data.Nikkei[i] - self.data.Nikkei[i - 1]) * sqrt(nikkei_var) / sqrt(
                 _data_pct.Nikkei_Var[i - 1])) / self.data.Nikkei[i - 1]
-        _data_pct2 -= 1.
-        _data_pct2 = _data_pct2.dropna()
+        _scenarios = _data_pct2 * self.today # Scenarios with volatility scaling for market variables
 
-        _scenarios = _data_pct2 * self.today + self.today
-        loss = (_scenarios * self.weights / self.today).sum(axis=1)
-        _scenarios['Loss'] = self.today_value - loss
-        var = _scenarios.Loss.quantile(.99)
-        es = _scenarios.Loss[_scenarios.Loss > var].mean()
+        portfolio_value_under_scenarios = (_scenarios * self.weights / self.today).sum(axis=1)
+        loss = self.today_value - portfolio_value_under_scenarios
+
+        loss.sort_values(ascending=False, inplace=True)
+        var = loss[4]
+        es = loss[:4].mean()
+
+        # Better approach that will work when the confidence level is parameterized
+        # var = loss.quantile(.99)
+        # es = loss[loss > var].mean()
+
+        print("Exercise 13.7 old. One day VaR with a confidence level of 99.0%% and \u03BB=%1.2f: %s"
+              % (lbd, locale.currency(var * self.scale_factor, grouping=True)))
+        print("Exercise 13.7 old. One day ES with a confidence level of 99.0%% and \u03BB=%1.2f: %s"
+              % (lbd, locale.currency(es * self.scale_factor, grouping=True)))
+
+        # Visualizing losses
+        x = np.arange(len(loss))
+        plt.plot(x, loss, label="Losses", linewidth=2, color='g')
+        plt.legend(loc='best')
+        plt.title('Sorted portfolio losses (using volatility scaling for market variables)')
+        plt.ylabel('Portfolio losses in $M')
+        plt.xlabel('Sorted scenarios')
+        plt.show()
+
+    def exercise_13_7(self, lbd):
+        loss = self.today_value - (self.scenarios * self.weights / self.today).sum(axis=1)
+        loss_variance = Series(loss.var(), loss.index)
+
+        # Unfortunately not vectorizable as the next value depends on the previous
+        # loss_variance.iloc[1:] = lbd * loss_variance.iloc[:-1] + (1 - lbd) * loss.iloc[:-1]**2
+        for i in range(1, len(loss_variance)):
+            loss_variance.iloc[i] = lbd * loss_variance[i - 1] + (1 - lbd) * loss[i - 1]**2
+
+        loss_std = loss_variance.apply(np.sqrt)
+        sd_ratio = loss_std[-1] / loss_std
+
+        # Adjusting the loss for the volatility of the portfolio
+        adjusted_loss = loss * sd_ratio
+
+        adjusted_loss.sort_values(ascending=False, inplace=True)
+        var = adjusted_loss[4]
+        es = adjusted_loss[:4].mean()
+
+        # Better approach that will work when the confidence level is parameterized, however produced answers slightly
+        # disagree from those in the textbook
+        # var = loss.quantile(.99)
+        # es = loss[loss > var].mean()
+
         print("Exercise 13.7. One day VaR with a confidence level of 99.0%% and \u03BB=%1.2f: %s"
               % (lbd, locale.currency(var * self.scale_factor, grouping=True)))
         print("Exercise 13.7. One day ES with a confidence level of 99.0%% and \u03BB=%1.2f: %s"
               % (lbd, locale.currency(es * self.scale_factor, grouping=True)))
+
+        # Visualizing losses
+        x = np.arange(len(loss))
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.plot(x, loss.sort_values(ascending=False), label="Losses", linewidth=1, color='b')
+        ax.plot(x, adjusted_loss, label="Adjusted losses", linewidth=2, color='g')
+        ax.legend(loc='best')
+        ax.set_title('Sorted portfolio losses and adjusted losses with volatility scaling for the portfolio')
+        ax.set_ylabel('Portfolio losses in $M')
+        ax.set_xlabel('Sorted scenarios')
+
 
 if __name__ == "__main__":
     import sys
@@ -142,6 +203,7 @@ if __name__ == "__main__":
         ch13.exercise_13_4([.95, .97])
         ch13.exercise_13_5([.99])
         ch13.exercise_13_6(lbd=.99)
+        ch13.exercise_13_7_old(lbd=.96)
         ch13.exercise_13_7(lbd=.96)
         from evt import Evt
         evt = Evt(hist_simulation_spreadsheet, 1e4, num_iter=30000)
